1f8692e7eb8ce5320aa5400fcacb682aca5f492a
shoavid <shoavid@gmail.com>
2024-03-22 16:05:54 +0200

    finishes part 2 before tests

--- malloc_2.cpp
+++ malloc_2.cpp
@@ -10,11 +10,15 @@ struct MallocMtadata{
 };
 
 struct MallocMtadata* ptr = nullptr;
+size_t num_free_blocks = 0;
+size_t num_free_bytes = 0;
+size_t num_allocated_blocks = 0;
+size_t num_allocated_bytes = 0;
 
 struct MallocMtadata* getFreeBlock(size_t size){
     struct MallocMtadata* temp = ptr;
     while (temp){
-        if (temp->is_free && temp->size <= sizeof(MallocMtadata)+size){
+        if (temp->is_free && temp->size <= _size_meta_data()+size){
             return temp;
         }
         temp = temp->next;
@@ -41,17 +45,19 @@ void* smalloc(size_t size){
     }
     struct MallocMtadata* block;
     if (!ptr){
-        block = (struct MallocMtadata*)sbrk(size);
+        block = (struct MallocMtadata*)sbrk(size + _size_meta_data());
         if (!block){
             return NULL;
         }
+        num_allocated_blocks++;
+        num_allocated_bytes += size;
         ptr = block;
         block->size = size;
     }
     else{
         block = getFreeBlock(size);
         if (!block){
-            block = (struct MallocMtadata*)sbrk(size);
+            block = (struct MallocMtadata*)sbrk(size + _size_meta_data());
             if (!block){
                 return NULL;
             }
@@ -59,10 +65,16 @@ void* smalloc(size_t size){
             last->next = block;
             block->prev =last;
             block->size = size;
+            num_allocated_blocks++;
+            num_allocated_bytes += size;
+        }
+        else{
+            num_free_blocks--;
+            num_free_bytes -= block->size;
         }
     }
     block->is_free = false;
-    return block + sizeof(MallocMtadata);
+    return block + sizeof(struct MallocMtadata);
 }
 
 
@@ -83,8 +95,11 @@ void sfree(void* p){
     if (!p){
         return;
     }
-    struct MallocMtadata* meta = (struct MallocMtadata*)p - sizeof(struct MallocMtadata);
+    struct MallocMtadata* meta = (struct MallocMtadata*)p - _size_meta_data();
     meta->is_free = true;
+    num_free_blocks++;
+    num_free_bytes += meta->size;
+
 }
 
 
@@ -92,7 +107,7 @@ void* srealloc(void* oldp, size_t size){
     if (size == 0 || size > pow(10, 8)){
         return NULL;
     }
-    struct MallocMtadata* oldMeta = (struct MallocMtadata*)oldp - sizeof(struct MallocMtadata);
+    struct MallocMtadata* oldMeta = (struct MallocMtadata*)oldp - _size_meta_data();
     if (oldMeta->size >= size){
         return oldp;
     }
@@ -103,4 +118,29 @@ void* srealloc(void* oldp, size_t size){
     memcpy(p, oldp, oldMeta->size);
     sfree(oldp);
     return p;
+}
+
+
+size_t _num_free_blocks(){
+    return num_free_blocks;
+}
+
+size_t _num_free_bytes(){
+    return num_free_bytes;
+}
+
+size_t _num_allocated_blocks(){
+    return num_allocated_blocks;
+}
+
+size_t _num_allocated_bytes(){
+    return num_allocated_bytes;
+}
+
+size_t _num_meta_data_bytes(){
+    return num_allocated_blocks * _size_meta_data();
+}
+
+size_t _size_meta_data(){
+    return sizeof(struct MallocMtadata);
 }
