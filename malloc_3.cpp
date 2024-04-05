43cfec930cd2e0ddcf7d42e036779aa446533abb
shoavid <shoavid@gmail.com>
2024-04-05 11:23:16 +0300

    stat functions includes mmap

--- malloc_3.cpp
+++ malloc_3.cpp
@@ -6,6 +6,73 @@
 #define ORDER_SIZE(i) (size_t)pow(2, i)*128
 
 
+
+
+size_t _num_free_bytes();
+size_t _num_free_blocks();
+size_t _num_allocated_bytes();
+size_t _num_overall_bytes();
+size_t _num_allocated_blocks();
+size_t _num_meta_data_bytes();
+size_t _size_meta_data();
+#define MAX_ALLOCATION_SIZE (1e8)
+#define MMAP_THRESHOLD (128 * 1024)
+#define MIN_SPLIT_SIZE (128)
+#define MAX_ELEMENT_SIZE (128*1024)
+#include <assert.h>
+#define REQUIRE(bool) assert(bool)
+#define verify_blocks(allocated_blocks, allocated_bytes, free_blocks, free_bytes)                                      \
+    do                                                                                                                 \
+    {                                                                                                                  \
+        REQUIRE(_num_allocated_blocks() == allocated_blocks);                                                          \
+        REQUIRE(_num_allocated_bytes() == (allocated_bytes));                                              \
+        REQUIRE(_num_free_blocks() == free_blocks);                                                                    \
+        REQUIRE(_num_free_bytes() == (free_bytes));                                                        \
+        REQUIRE(_num_meta_data_bytes() == (_size_meta_data() * allocated_blocks));                         \
+    } while (0)
+
+#define verify_size(base)                                                                                              \
+    do                                                                                                                 \
+    {                                                                                                                  \
+        void *after = sbrk(0);                                                                                         \
+        REQUIRE(_num_allocated_bytes() + aligned_size(_size_meta_data() * _num_allocated_blocks()) ==                  \
+                (size_t)after - (size_t)base);                                                                         \
+    } while (0)
+
+#define verify_size_with_large_blocks(base, diff)                                                                      \
+    do                                                                                                                 \
+    {                                                                                                                  \
+        void *after = sbrk(0);                                                                                         \
+        REQUIRE(diff == (size_t)after - (size_t)base);                                                                 \
+    } while (0)
+
+void verify_block_by_order(int order0free, int order0used, int order1free, int order1used, \
+                                int order2free, int order2used,\
+                                int order3free, int order3used, \
+                                int order4free, int order4used, \
+                                int order5free, int order5used, \
+                                int order6free, int order6used, \
+                                int order7free, int order7used, \
+                                int order8free,int  order8used, \
+                                int order9free,int  order9used, \
+                                int order10free,int  order10used,
+                                int big_blocks_count, long big_blocks_size  )\
+                                                                                                                     \
+    {                                                                                                                  \
+        unsigned int __total_blocks = order0free + order0used+ order1free + order1used+ order2free + order2used+ order3free + order3used+ order4free + order4used+ order5free + order5used+ order6free + order6used+ order7free + order7used+ order8free + order8used+ order9free + order9used+ order10free + order10used + big_blocks_count       ;        \
+        unsigned int __total_free_blocks = order0free+ order1free+ order2free+ order3free+ order4free+ order5free+ order6free+ order7free+ order8free+ order9free+ order10free ;                     \
+        unsigned int __total_free_bytes_with_meta  = order0free*128*pow(2,0) +  order1free*128*pow(2,1) +  order2free*128*pow(2,2) +  order3free*128*pow(2,3) +  order4free*128*pow(2,4) +  order5free*128*pow(2,5) +  order6free*128*pow(2,6) +  order7free*128*pow(2,7) +  order8free*128*pow(2,8) +  order9free*128*pow(2,9)+  order10free*128*pow(2,10) ;                                                                     \
+        unsigned int testing_allocated_bytes;
+        if (__total_blocks==0) testing_allocated_bytes = 0;
+        else testing_allocated_bytes = big_blocks_size+32 * MAX_ELEMENT_SIZE - (__total_blocks-big_blocks_count)*(_size_meta_data());
+        verify_blocks(__total_blocks, testing_allocated_bytes, __total_free_blocks,__total_free_bytes_with_meta - __total_free_blocks*(_size_meta_data()));\
+    }
+
+
+
+
+
+
 struct MallocMtadata{
     size_t size;
     bool is_free;
@@ -57,8 +124,10 @@ size_t _num_meta_data_bytes(){
 
 void align_memory(){
     void* ptr = sbrk(0);
-    size_t x = ((size_t)ptr) % (32*128*1024);
-    sbrk(32*128*1024 - x);
+    size_t x = ((size_t)ptr) % (size_t)(32*128*1024);
+    if (x != 0){
+        sbrk((int)((size_t)(32*128*1024) - x));
+    }
 }
 
 
@@ -68,13 +137,13 @@ void initialHeap(){
     num_allocated_blocks = 32;
     num_free_bytes = (size_t)32*(ORDER_SIZE(MAX_ORDER) - _size_meta_data());
     num_overall_bytes += ORDER_SIZE(MAX_ORDER)*32;
-    heap_ptr = (struct MallocMtadata*)sbrk(ORDER_SIZE(MAX_ORDER)*32);
+    heap_ptr = (struct MallocMtadata*)sbrk(32*128*1024);
     orders[10] = heap_ptr;
     struct MallocMtadata* temp = heap_ptr;
     struct MallocMtadata* tempPrev;
     for (int i = 0; i<31; i++){
         //temp->next = temp + ORDER_SIZE(MAX_ORDER);
-        temp->next_order = temp + ORDER_SIZE(MAX_ORDER);
+        temp->next_order = (struct MallocMtadata*)((size_t)temp + (size_t)ORDER_SIZE(MAX_ORDER));
         temp->size = ORDER_SIZE(MAX_ORDER);
         temp->is_free = true;
         tempPrev = temp;
@@ -183,6 +252,8 @@ void* smalloc(size_t size){
         meta->size = size; //check
         meta->is_free = false;
         addToMmap(meta);
+        num_allocated_blocks++;
+        num_overall_bytes += size + _size_meta_data();
         return (struct MallocMtadata*)((size_t)meta + _size_meta_data());
     }
     return getFreeBlock(size);
@@ -249,6 +320,8 @@ void sfree(void* p){ //free mmap!
         return;
     }
     if (meta->size + _size_meta_data() >= ORDER_SIZE(MAX_ORDER)){
+        num_allocated_blocks--;
+        num_overall_bytes -= meta->size + _size_meta_data();
         outOfOrder(meta, false);
         munmap(meta, meta->size + _size_meta_data());
         return;
@@ -321,4 +394,49 @@ void* srealloc(void* oldp, size_t size){
     memcpy(p, oldp, oldMeta->size);
     sfree(oldp);
     return p;
+}
+
+
+
+
+int main(){
+    // Initial state
+    verify_block_by_order(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+
+    // Allocate small block (order 0)
+    void *ptr1 = smalloc(40);
+    REQUIRE(ptr1 != nullptr);
+//    verify_size(base);
+    verify_block_by_order(1,1,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,31,0,0,0);
+
+    // Allocate large block (order 10)
+    void *ptr2 = smalloc(MAX_ELEMENT_SIZE+100);
+    REQUIRE(ptr2 != nullptr);
+//    verify_size_with_large_blocks(base, (128 * 1024+100 +_size_meta_data()));
+    verify_block_by_order(1,1,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,31,0,1,MAX_ELEMENT_SIZE+100);
+
+    // Allocate another small block
+    void *ptr3 = smalloc(50);
+    REQUIRE(ptr3 != nullptr);
+    verify_block_by_order(0,2,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,31,0,1,MAX_ELEMENT_SIZE+100);
+
+    // Free the first small block
+    sfree(ptr1);
+    verify_block_by_order(1,1,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,31,0,1,MAX_ELEMENT_SIZE+100);
+
+
+    // Allocate another small block
+    void *ptr4 = smalloc(40);
+    REQUIRE(ptr4 != nullptr);
+    verify_block_by_order(0,2,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,31,0,1,MAX_ELEMENT_SIZE+100);
+
+    // Free all blocks
+    sfree(ptr3);
+    sfree(ptr4);
+    verify_block_by_order(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,32,0,1,MAX_ELEMENT_SIZE+100);
+    sfree(ptr1); //free again
+    sfree(ptr2);
+    verify_block_by_order(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,32,0,0,0);
+//    verify_size_with_large_blocks(base, 0);
+//    verify_size(base);
 }
